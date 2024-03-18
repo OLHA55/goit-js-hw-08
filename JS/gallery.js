6ab566bae21ad13b9f21dd3c68134da335fb2577
OLHA55 <bagira2310@gmail.com>
2024-03-18 22:23:25 +0200

    js08neew03

--- JS/gallery.js
+++ JS/gallery.js
@@ -63,4 +63,47 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+const container = document.querySelector(".gallery");
+container.insertAdjacentHTML("beforeend", createPreview(images));
+container.addEventListener(`click`, hendleImageclick);
+console.log();
 
+
+
+// function createPreview(arr) {
+//   return arr
+//     .map((images) => `
+//     <img class="gallery-image" src="small-image.jpg" data-source="large-image.jpg"
+//     <li class="gallery-item">
+//      <a class="gallery-link" href="large-image.jpg"> alt="Image description"></a>
+//     </li>
+//    `).Join("");
+// }
+//    console.log(createMarkup(images));
+
+// // function hendleImageclick(event) {
+// //   // console.log(event.targetven,event.carrentTarget);
+// //   if (event.targetven === event.carrentTarget) {
+// //     return;
+//   }
+//   const carrentImage = event.target.closest(".image-item");
+//  // console.log(event.carretTarget);
+//  // console.log(event.target);
+// //   console.log(carrentProduct);
+//   const imageId = carrentImage.dataset.id; 
+//   const image = image.find(item => item.id === Number(imageId));
+
+//   const instance = basicLightbox.create(`
+//   <div class ="modal">
+//    <img src="${image.img}" alt="${image.img}">
+//    <h2>${image. description}</h2>
+   
+//   </div>
+//   `)
+//   instance.show();
+//   }
+  
+//   // console.log(productId);
+//   // console.log(typeof productId);
+//  //console.log(product);
+//  //console.log(window);
